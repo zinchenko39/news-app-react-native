cc6567033cd6c31e96ab1cba975afef1a930dfdb
Vadim <vadim.zinchenco@mail.ru>
2022-11-06 14:49:10 +0400

    Add component Post

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+  View, StatusBar, FlatList, Alert, ActivityIndicator,
+} from 'react-native';
+import styled from 'styled-components/native';
+import { Post } from './components/Post';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+
+  const fetchPosts = () => {
+    setIsLoading(true);
+    axios
+      .get('https://6367830ff5f549f052d6c78f.mockapi.io/news')
+      .then(({ data }) => {
+        setItems(data);
+      })
+      .catch((error) => {
+        Alert.alert('Ошибка', 'Не удалось получить статьи');
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ActivityIndicator size="large" />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <Post title={item.title} imageUrl={item.imageUrl} createdAt={item.createdAt} />}
+      />
+      <StatusBar theme="auto" />
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Post.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components/native";
+
+const PostView = styled.View`
+  flex-direction: row;
+  margin-top: 30px;
+  padding: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0 0.1);
+  border-bottom-styled: solid;
+`;
+
+const PostImage = styled.Image`
+  width: 60px;
+  heigth: 60px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const PostTitle = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+`;
+
+const PostDetails = styled.View`
+  justify-content: center;
+  flex: 1;
+`;
+
+const PostData = styled.Text`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`;
+
+export const Post = ({ title, imageUrl, createdAt }) => {
+  return (
+    <PostView>
+      <PostImage source={{ uri: imageUrl }} />
+      <PostDetails>
+        <PostTitle>{title}</PostTitle>
+        <PostData>{createdAt}</PostData>
+      </PostDetails>
+    </PostView>
+  );
+};
